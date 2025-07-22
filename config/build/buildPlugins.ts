66886e10146f25c8a82488d293fc14475e87e85e
Anton Mataev <Geolog332@yahoo.com>
2025-07-22 22:29:53 +0500

    first commit

---
+++ config/build/buildPlugins.ts
@@ -0,0 +1,18 @@
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import webpack from "webpack";
+import { BuildOptions } from "./types/config";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+
+export function buildPlugins(options: BuildOptions) {
+    
+    return [
+        new HtmlWebpackPlugin({
+            template: options.paths.html,
+        }),
+        new webpack.ProgressPlugin(),
+        new MiniCssExtractPlugin({
+            filename: "css/[name].[contenhash:8].css", 
+            chunkFilename: "css/[name].[contenhash:8].css"
+        })
+    ]
+}
