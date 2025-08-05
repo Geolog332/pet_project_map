f7e302d2fef88d5086af930693b40102bf3764a9
Anton Mataev <Geolog332@yahoo.com>
2025-08-05 23:22:38 +0500

    fix action github

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -4,20 +4,28 @@ import { BuildOptions } from "./types/config";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import {BundleAnalyzerPlugin} from 'webpack-bundle-analyzer';
 
-export function buildPlugins(options: BuildOptions) {
-    
-    return [
+export function buildPlugins({paths, isDev}: BuildOptions): webpack.WebpackPluginInstance[] {
+    const plugins = [
         new HtmlWebpackPlugin({
-            template: options.paths.html,
+            template: paths.html,
         }),
         new webpack.ProgressPlugin(),
         new MiniCssExtractPlugin({
             filename: "css/[name].[contenhash:8].css", 
             chunkFilename: "css/[name].[contenhash:8].css"
         }),
-        new webpack.HotModuleReplacementPlugin(),
-        new BundleAnalyzerPlugin({
-            openAnalyzer: false, // Disable automatic opening of the report
+    ];
+
+    if (isDev) {
+        plugins.push(        
+    new webpack.HotModuleReplacementPlugin(),
+    new BundleAnalyzerPlugin({
+        openAnalyzer: false, // Disable automatic opening of the report
         }), 
-    ]
+    )
+    }
+
+
+
+    return plugins;
 }
